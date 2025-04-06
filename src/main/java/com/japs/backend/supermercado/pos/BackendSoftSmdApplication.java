5f3746cfe0c2a674a07924d19899d40bbd5815ca
Jhonathan Peñaloza <jpenaloz@nequi.com>
2025-04-06 16:59:24 -0500

    primer commit

---
+++ src/main/java/com/japs/backend/supermercado/pos/BackendSoftSmdApplication.java
@@ -0,0 +1,13 @@
+package com.japs.backend.supermercado.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSoftSmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSoftSmdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/japs/backend/supermercado/pos/BackendSoftSmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japs.backend.supermercado.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSoftSmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
